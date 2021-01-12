c1ffcecc91c5b28bb661a412faa4055058898da6
Jeff Nyman <jeffnyman@gmail.com>
2021-01-12 08:19:53 -0600

    Ability to write disassembly instructions.

--- src/chip8_dasm/writer.py
+++ src/chip8_dasm/writer.py
@@ -1,10 +1,14 @@
 """Writer implementation for CHIP-8 ROM disassemebly."""
 
+from typing import Tuple
+
 from chip8_dasm.disassembler import Disassembler
 
 
 class Writer:
     """Simple abstraction for a disassembly writer."""
+
+    STARTING_ADDRESS = 0x200
 
     def __init__(self, dasm: Disassembler):
         self.dasm = dasm
@@ -12,4 +16,35 @@ class Writer:
     def generate(self) -> None:
         """Write out disassembly information."""
 
-        print(self.dasm.disassembly)
+        dasm_output = self.generate_disassembly_buffer(self.STARTING_ADDRESS)
+
+        print(dasm_output)
+
+    def generate_disassembly_buffer(self, address: int) -> str:
+        """Create buffer structure for disassembly data."""
+
+        dasm_buffer = "start:\n"
+
+        while address < self.end_rom_file():
+            line, address = self.generate_instructions(address)
+            dasm_buffer += line
+
+        return dasm_buffer
+
+    def generate_instructions(self, address: int) -> Tuple[str, int]:
+        """Iterate through all instructions for the disassembly buffer."""
+
+        line = ""
+
+        if address in self.dasm.disassembly:
+            line = "     {}\n".format(self.dasm.disassembly[address])
+            address += 2
+        else:
+            address += 1
+
+        return (line, address)
+
+    def end_rom_file(self) -> int:
+        """Return the length of the non-interpreter portion of ROM."""
+
+        return self.STARTING_ADDRESS + len(self.dasm.rom_data)
